97757ec5370226e95f93772ea312882a30b31e03
Katarzyna Tabor <katarzyna.tabor@gmail.com>
2022-09-21 12:27:58 +0200

    Dodanie usuwanie katalogi jeśli istnieje

--- src/html/htmls_generator.py
+++ src/html/htmls_generator.py
@@ -1,13 +1,18 @@
 import os
+import shutil
 
 import src.html.create_songs_html as cash
+
 
 def main():
     target_dir = os.path.join("..", "..", "build")
     src = os.path.join("..", "..", "songs")
     path_songs_html = os.path.join(target_dir, "songs_html")
-    os.mkdir(path_songs_html)
+    if os.path.exists(path_songs_html):
+        shutil.rmtree(path_songs_html)
+        os.mkdir(path_songs_html)
     cash.create_all_songs_html(src, src, path_songs_html)
+
 
 if __name__ == "__main__":
     main()
