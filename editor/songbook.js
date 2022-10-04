60722a1a68771beb2542da8ac2cb5e9482a55b46
Piotr Tabor <ptab@google.com>
2022-10-04 10:52:40 +0200

    Delete button more or less working

--- editor/songbook.js
+++ editor/songbook.js
@@ -3,7 +3,6 @@ let dropped = null;
 
 function createChord(chord) {
   ch = document.createElement("song-ch");
-  //akord.className = 'akord';
   ch.draggable = true;
   ch.ondragstart = (e) => {
     e.target.opacity = "0.2";
@@ -17,11 +16,11 @@ function createChord(chord) {
     if (dropped && e.dataTransfer.dropEffect == 'move') {
       e.target.remove();
     }
-    console.warn(e);
   }
   ch.setAttribute('a', chord);
   ch.draggable = true;
   ch.style.userSelect='all';
+  return ch;
   return ch;
 }
 
@@ -40,16 +39,15 @@ function getRangeForCursor(e) {
 }
 
 function acceptsTextAndChords(element) {
-  return element != null && (element.nodeName==='SONG-VERSE'
-    || element.nodeName==="SONG-BIS"
-    || (element.classList != null && element.classList.contains("accepts-ch")));
+  return ((element.classList != null) && element.classList.contains("accepts-ch"));
 }
 
 function rowOnDrop(e) {
   d = e.dataTransfer.getData("songbook/chord");
   if (d != null && d != "") {
     range = getRangeForCursor(e)
-    if (acceptsTextAndChords(range.commonAncestorContainer.parentNode)) {
+    a=acceptsTextAndChords(range.commonAncestorContainer.parentNode);
+    if (a) {
       range.insertNode(createChord(d));
       dropped = true;
     }
@@ -61,11 +59,11 @@ function canInsertChord() {
   //console.warn(window.getSelection().getRangeAt(0));
   if (window.getSelection().rangeCount < 1) {return false; }
   let r = window.getSelection().getRangeAt(0);
-  console.warn(r.startContainer);
+ // console.warn(r.startContainer);
   let x =
-      acceptsTextAndChords(r.startContainer)
-      || (r.startContainer.nodeName == '#text'
-          && acceptsTextAndChords(r.startContainer.parentNode));
+      //acceptsTextAndChords(r.startContainer);
+      (r.startContainer.nodeName == '#text'
+           && acceptsTextAndChords(r.startContainer.parentNode));
   return x;
 }
 
@@ -98,8 +96,18 @@ function rowOnDragOver(e) {
     range = getRangeForCursor(e);
     document.getSelection().removeAllRanges();
     document.getSelection().addRange(range);
+    if (!canInsertChord()) {
+      e.dataTransfer.dropEffect = 'none';
+    }
     e.preventDefault();
   }
+}
+
+function setCursorBefore(node) {
+  let newr=document.createRange();
+  newr.setStartBefore(node);
+  document.getSelection().removeAllRanges();
+  document.getSelection().addRange(newr);
 }
 
 function createLyric() {
@@ -116,70 +124,92 @@ function createLyric() {
     }
   };
   rowp.oninput = function (e) {
+    console.log("Oninput", e);
    // sanitize(e.target);
   }
   rowp.onbeforeinput = function (e) {
-    if (e.inputType == "insertParagraph") {
-      //document.getSelection().getRangeAt(0).insertNode(document.createElement("br"));
-      if (document.getSelection().isCollapsed
-          && document.getSelection().rangeCount == 1
-          && document.getSelection().getRangeAt(0).startContainer.nodeName==='#text'
-          && document.getSelection().getRangeAt(0).startContainer.nodeValue[document.getSelection().getRangeAt(0).startOffset]===' ') {
-        // Don't put additional space if we break just ahead of 'space'
-        document.execCommand("insertHTML", false, '<br/>')
-      } else {
-        document.execCommand("insertHTML", false, '<br/> ')
-      }
-      e.preventDefault();
-    }
+    // if (e.inputType == "insertParagraph") {
+    //   //document.getSelection().getRangeAt(0).insertNode(document.createElement("br"));
+    //   if (document.getSelection().isCollapsed
+    //       && document.getSelection().rangeCount == 1
+    //       && document.getSelection().getRangeAt(0).startContainer.nodeName==='#text'
+    //       && document.getSelection().getRangeAt(0).startContainer.nodeValue[document.getSelection().getRangeAt(0).startOffset]===' ') {
+    //     // Don't put additional space if we break just ahead of 'space'
+    //     document.execCommand("insertHTML", false, '<br/>')
+    //   } else {
+    //     document.execCommand("insertHTML", false, '<br/>&nbsp;')
+    //   }
+    //   e.preventDefault();
+    // }
     if (e.inputType == "deleteContentBackward") {
       if (document.getSelection().rangeCount == 1
           && document.getSelection().isCollapsed) {
         let r = document.getSelection().getRangeAt(0);
-        // If the previous element is akord, we want to skip and remove the prev letter.
+        // If the previous element is chord, we want to skip and remove the prev letter.
         if (r.startContainer.nodeName == '#text' && r.startOffset == 0
-            && r.startContainer.previousSibling != null && r.startContainer.previousSibling.className==='akord') {
-          // If the letter is BR, we need to remove it in a special way.
-          if (r.startContainer.previousSibling.previousSibling.nodeName === 'BR') {
-            let newr=document.createRange();
-            newr.selectNode(r.startContainer.previousSibling.previousSibling);
-            newr.deleteContents();
-            // document.getSelection().removeAllRanges();
-            // document.getSelection().addRange(newr);
-            //
-            // // document.getSelection().collapse(r.startContainer.previousSibling.previousSibling);
-            // document.execCommand("insertHTML",false,"Foo");
-            e.preventDefault();
-            return;
-          } else {
-            let newr=document.createRange();
-            newr.setStartBefore(r.startContainer.previousSibling);
-            document.getSelection().removeAllRanges();
-            document.getSelection().addRange(newr);
-            e.preventDefault();
-            return;
-          }
-        } else if (r.startContainer.nodeName == '#text' && r.startOffset <= 1
-               &&  r.startContainer.previousSibling.nodeName==='BR') {
-          let newr=document.createRange();
-          newr.setStartBefore(r.startContainer.previousSibling);
-          newr.setEnd(r.startContainer, r.startOffset);
-          newr.deleteContents();
-          e.preventDefault();
-        } else if (r.startContainer.nodeName == '#text' && r.startOffset == 0
-            &&  r.startContainer.previousSibling.nodeName==='#text') {
-          let newr=document.createRange();
-          newr.setEndBefore(r.startContainer);
-          newr.setStart(r.startContainer.previousSibling, r.startContainer.previousSibling.length - 1);
-          console.log(newr);
-          newr.deleteContents();
-          e.preventDefault();
+            && r.startContainer.previousSibling != null && r.startContainer.previousSibling.nodeName==='SONG-CH') {
+          setCursorBefore(r.startContainer.previousSibling);
+          r = document.getSelection().getRangeAt(0);
+          // We want to continue in case we want to merge rows.
         }
+        if (r.startContainer.nodeName=='SONG-ROW' && r.startOffset==0) {
+          if (r.startContainer.previousSibling != null
+              && r.startContainer.previousSibling.nodeName ==='SONG-ROW') {
+            let first=r.startContainer.childNodes[0];
+            let documentFragment = document.createDocumentFragment();
+            while (r.startContainer.childNodes.length>0) {
+              documentFragment.appendChild(r.startContainer.childNodes[0]);
+            }
+            r.startContainer.previousSibling.appendChild(documentFragment);
+            setCursorBefore(first);
+            e.preventDefault();
+          }
+        }
+
+        //console.log(r, r.startContainer, r.startContainer.nodeName, r.startOffset);
+        //   // If the letter is BR, we need to remove it in a special way.
+        //   if (r.startContainer.previousSibling.previousSibling.nodeName === 'BR') {
+        //     let newr=document.createRange();
+        //     newr.selectNode(r.startContainer.previousSibling.previousSibling);
+        //     newr.deleteContents();
+        //     // document.getSelection().removeAllRanges();
+        //     // document.getSelection().addRange(newr);
+        //     //
+        //     // // document.getSelection().collapse(r.startContainer.previousSibling.previousSibling);
+        //     // document.execCommand("insertHTML",false,"Foo");
+        //     e.preventDefault();
+        //     return;
+        //   } else {
+        //     let newr=document.createRange();
+        //     newr.setStartBefore(r.startContainer.previousSibling);
+        //     document.getSelection().removeAllRanges();
+        //     document.getSelection().addRange(newr);
+        //     e.preventDefault();
+        //     return;
+        //   }
+        // } else if (r.startContainer.nodeName == '#text' && r.startOffset <= 1
+        //        &&  r.startContainer.previousSibling.nodeName==='BR') {
+        //   let newr=document.createRange();
+        //   newr.setStartBefore(r.startContainer.previousSibling);
+        //   newr.setEnd(r.startContainer, r.startOffset);
+        //   newr.deleteContents();
+        //   e.preventDefault();
+        // } else if (r.startContainer.nodeName == '#text' && r.startOffset == 0
+        //     &&  r.startContainer.previousSibling.nodeName==='#text') {
+        //   let newr=document.createRange();
+        //   newr.setEndBefore(r.startContainer);
+        //   newr.setStart(r.startContainer.previousSibling, r.startContainer.previousSibling.length - 1);
+        //   console.log(newr);
+        //   newr.deleteContents();
+        //   e.preventDefault();
+        // }
           //   && r.startContainer.previousSibling != null && r.startContainer.previousSibling.className==='akord') {
           //console.log("START", r.startContainer, r.startOffset, r.startContainer.previousSibling, r.extractContents())
         //}
       }
     }
+    let r = document.getSelection().getRangeAt(0);
+    console.log(r, r.startContainer, r.startContainer.nodeName, r.startOffset);
     //console.log("BEFORE", e,document.getSelection());
     //console.dir(JSON.stringify(document.getSelection(), null, 4) );
   }
@@ -194,40 +224,45 @@ function createLyric() {
 
 
 function  rowToNodes(row) {
+  let newRow = document.createElement("song-row");
+  newRow.classList.add("accepts-ch");
   let nodes=[];
   for (let i = 0; i < row.childNodes.length; ++i) {
     let node = row.childNodes[i];
     if (node.nodeName == '#text') {
-      nodes.push(node.cloneNode());
+      let n = node.cloneNode();
+      newRow.appendChild(n);
+      let regex = / /g;
+      n.nodeValue = n.nodeValue.replaceAll(regex, "\u00a0");
     } else {
-      nodes.push(createChord(node.attributes['a'].value));
+      newRow.appendChild(createChord(node.attributes['a'].value));
     }
   }
-  return nodes;
+  return [newRow];
 }
 
-function sanitize(lyric) {
-  for (let i=0; i < lyric.childNodes.length; ++i) {
-    let node = lyric.childNodes[i]
-    let parent = node.parentNode
-    if (node.nodeName=='DIV') {
-      let next = node.nextSibling;
-      for (let i = node.childNodes.length - 1; i>=0; --i) {
-        if (next) {
-          next = parent.insertBefore( node.childNodes[i], next);
-
-        } else {
-          console.log(node.childNodes[i])
-          next = parent.appendChild(node.childNodes[i]);
-        }
-      }
-      if (next) {
-        parent.insertBefore(document.createElement("br"), next);
-      }
-      node.remove();
-    }
-  }
-}
+// function sanitize(lyric) {
+//   for (let i=0; i < lyric.childNodes.length; ++i) {
+//     let node = lyric.childNodes[i]
+//     let parent = node.parentNode
+//     if (node.nodeName=='DIV') {
+//       let next = node.nextSibling;
+//       for (let i = node.childNodes.length - 1; i>=0; --i) {
+//         if (next) {
+//           next = parent.insertBefore( node.childNodes[i], next);
+//
+//         } else {
+//           console.log(node.childNodes[i])
+//           next = parent.appendChild(node.childNodes[i]);
+//         }
+//       }
+//       if (next) {
+//         parent.insertBefore(document.createElement("br"), next);
+//       }
+//       node.remove();
+//     }
+//   }
+// }
 
 function onLoad() {
 
@@ -258,13 +293,13 @@ function onLoad() {
     let verse = verses[vi];
     let songVerse = document.createElement("song-verse");
     let d= document.createElement("div");
-    d.classList.add("accepts-ch")
+ //   d.classList.add("accepts-ch")
     lyric.appendChild(songVerse);
     songVerse.appendChild(d);
     let rows = verse.getElementsByTagName('row');
     for (let i = 0; i < rows.length; ++i) {
       d.append(...rowToNodes(rows[i]));
-      d.append(document.createElement("br"))
+   //   d.append(document.createElement("br"))
     }
 
     let san = document.createElement("span");
