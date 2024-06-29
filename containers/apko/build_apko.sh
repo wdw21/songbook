84e9b5fb1fd49bc22051fa86da596f4e0b633df4
Piotr Tabor <piotr.tabor@gmail.com>
2024-06-29 22:52:57 +0200

    Update container reference.

--- containers/apko/build_apko.sh
+++ containers/apko/build_apko.sh
@@ -9,12 +9,16 @@ docker run -v "$PWD":/work cgr.dev/chainguard/apko build /work/latex.apko.yaml l
 docker load -i latex.tar
 docker build -t ${CONTAINER} .
 
-docker run --rm -v "${PWD}/../..:/work" -w /work -it ${CONTAINER} ./render_pdf.sh songbook a4 test ./songs/alpuhara.xml
-docker push "${CONTAINER}"
+
+docker run --rm -v "${PWD}/../..:/work" -w /work -it ${CONTAINER} /bin/bash
+
+#docker run --rm -v "${PWD}/../..:/work" -w /work -it ${CONTAINER} ./render_pdf.sh songbook a4 test ./songs/alpuhara.xml
+
+
+#docker push "${CONTAINER}"
 
 
 # europe-docker.pkg.dev
-
-gcloud auth login piotr.tabor@gmail.com
-gcloud config set project wdw-21
-gcloud auth configure-docker europe-docker.pkg.dev
+#gcloud auth login piotr.tabor@gmail.com
+#gcloud config set project wdw-21
+#gcloud auth configure-docker europe-docker.pkg.dev
