9acf0614b3802378c56e5e2e9dcc9267ea45a4e4
Piotr Tabor <piotr.tabor@gmail.com>
2024-07-16 22:14:28 +0200

    Add epubcheck.

--- render_epubs.sh
+++ render_epubs.sh
@@ -6,5 +6,9 @@ for s in ${SCRIPT_DIR}/songbooks/*.yaml; do
   echo "Rendering: ${s}" >&2
   SONGBOOK_ID=$(PYTHONPATH="${SCRIPT_DIR}" python3 ${SCRIPT_DIR}/src/songbook2id.py "${s}")
   ${SCRIPT_DIR}/render_epub.sh "${s}"
-  epubcheck "${SCRIPT_DIR}/build/${SONGBOOK_ID}.epub"
+  if /opt/homebrew/bin/epubcheck; then
+    epubcheck "${SCRIPT_DIR}/build/${SONGBOOK_ID}.epub"
+  else
+    java -jar /usr/bin/epubcheck "${SCRIPT_DIR}/build/${SONGBOOK_ID}.epub"
+  fi
 done
