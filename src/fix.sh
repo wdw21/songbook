fa5cbed308480b43126a76c162a86bcb22def60c
Piotr Tabor <piotr.tabor@gmail.com>
2024-09-28 23:14:45 +0200

    Corrected normalization code.

--- src/fix.sh
+++ src/fix.sh
@@ -12,7 +12,7 @@ sed -i.bak "s|\"Stare Żbiki\"|„Stare Żbiki”|g" $1
 sed -i.bak "s|,,Stare Żbiki&#039;&#039;|„Stare Żbiki”|g" $1
 sed -i.bak "s|21. WDH „Stare Żbiki”|21. WDW „Stare Żbiki”|g" $1
 
-sed -i.bak "s|Zejman i Garkumpel|Zejman \& Garkumpel|g" $1
+sed -i.bak "s|Zejman i Garkumpel|Zejman \&amp; Garkumpel|g" $1
 sed -i.bak "s|Trzy dni Tyłem|Trzy Dni Tyłem|g" $1
 
 sed -i.bak "s|Aleksander Grotowski, Małgorzata Zwierzchowska|Aleksander Grotowski; Małgorzata Zwierzchowska|g" $1
